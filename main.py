b1f033c354689f81cfe0979e5739ee771bb7a596
Jeehyun Yoon (Peter) <dbswlgus0203@gmail.com>
2023-10-15 20:38:07 -0400

    I add summarizingand explanation functoin

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import openai
 import os
 from pydub import AudioSegment
+import requests
 
 # Set your OpenAI API key here
 openai.api_key = "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"
 
 # Define the path to your audio file
-audio_file_path = r"C:\Users\Peter\Desktop\julia.m4a"
+audio_file_path = r"C:\Users\Peter\Desktop\julia2.m4a"
 
 # Function to transcribe audio using OpenAI API
 def transcribe_audio(audio_data):
@@ -20,12 +21,23 @@ def save_transcriptions_to_file(transcriptions, output_file):
             file.write(str(transcription) + "\n\n")  # Convert the OpenAI object to a string
 
 def summarize_text(text):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=f"Summarize the following text: '{text}'",
-        max_tokens=50  # You can adjust the max_tokens for the desired length of the summary
-    )
-    return response.choices[0].text
+    api_url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {openai.api_key}"
+    }
+    payload = {
+        "model": "gpt-4",
+        "messages": [{"role": "user", "content": f"Can you create notes for each section and make long,specific, detailed explaination on each section: '{text}'"}],
+        "temperature": 0.7
+    }
+
+    response = requests.post(api_url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        return response.json()["choices"][0]["message"]["content"]
+    else:
+        return f"Error: {response.status_code} - {response.text}"
 
 def divide_into_chunks(audio):
     target_chunk_size_bytes = 25 * 1024 * 1024  # 25MB
@@ -55,6 +67,7 @@ def main():
                 transcript = transcribe_audio(audio_file)
                 # Save the transcription to a text file
                 save_transcriptions_to_file([transcript], "transcriptions.txt")
+            print(summarize_text(transcript))
         else:
             # Load the large audio file using PyDub
             audio = AudioSegment.from_file(audio_file_path)
@@ -81,8 +94,6 @@ def main():
 
             # Save all transcriptions to a text file
             save_transcriptions_to_file(transcriptions, "transcriptions.txt")
-            
-        print(summarize_text())
 
         print("Audio processing and transcription completed.")
     except Exception as e:
